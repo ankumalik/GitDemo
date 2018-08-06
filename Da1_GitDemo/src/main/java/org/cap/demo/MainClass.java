810bec8fb1244dd70a97c200aa44372ba3538d13
anmalik <anmalik@DIN73002975.corp.capgemini.com>
2018-08-06 10:53:03 +0530

    fro loop implimented

--- Da1_GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Da1_GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -1,10 +1,19 @@
 package org.cap.demo;
 
-public class MainClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class MainClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Yoo.!! BBye.!!");
 	}
-
+	
+	public static void greet()
+	{
+		System.out.println("Heel-o");
+		
+		for(int i=1; i<10; i++)
+		{
+			System.out.println(i+" ");
+		}
+	}
 }
